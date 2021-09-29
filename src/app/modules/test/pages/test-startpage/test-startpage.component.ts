242c2e886fae561ce239bedc8e550dab756e2cb6
Simon Sharp <simon.sharp@elca.ch>
2021-09-29 11:04:46 +0200

    Create Testpage

--- src/app/modules/test/pages/test-startpage/test-startpage.component.ts
+++ src/app/modules/test/pages/test-startpage/test-startpage.component.ts
@@ -1,5 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {TableColumn} from '../../../shared/model/table-column.class';
+import {FormSectionDeclaration} from '../../../shared/model/form-section-declaration.class';
+import {FormFieldDeclaration} from '../../../shared/model/form-field-declaration.class';
+import {SharedFormInputComponent} from '../../../shared/components/shared-form-input/shared-form-input.component';
+import {DynamicComponent} from '../../../shared/model/dynamic-component.class';
 
 @Component({
   selector: 'app-test-startpage',
@@ -9,9 +13,13 @@ export class TestStartpageComponent implements OnInit {
   public tableColumns: TableColumn[] = [];
   public tableData: any[] = [];
 
+  public formSections: FormSectionDeclaration[] = [];
+
   public ngOnInit(): void {
     this.initTableColumns();
     this.initTableData();
+
+    this.initForm();
   }
 
   public initTableColumns(): void {
@@ -49,6 +57,35 @@ export class TestStartpageComponent implements OnInit {
         vorname: 'Scarlet',
         nachname: 'Johansson',
       }
+    ];
+  }
+
+  public initForm(): void {
+    this.formSections = [
+      new FormSectionDeclaration({
+        formFields: [
+          new FormFieldDeclaration({
+            formComponent: new DynamicComponent<SharedFormInputComponent>({
+              componentType: SharedFormInputComponent,
+              inputs: {
+                label: 'Vorname',
+                name: 'vorname',
+              }
+            }),
+            bootstrapWidth: 6,
+          }),
+          new FormFieldDeclaration({
+            formComponent: new DynamicComponent<SharedFormInputComponent>({
+              componentType: SharedFormInputComponent,
+              inputs: {
+                label: 'Nachname',
+                name: 'nachname',
+              }
+            }),
+            bootstrapWidth: 6,
+          })
+        ]
+      })
     ];
   }
 }
