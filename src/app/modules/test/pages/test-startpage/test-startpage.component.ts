78c51af13c0d823c72484dbedf3fddf0594abe4e
Simon Sharp <simon.sharp@elca.ch>
2021-09-29 10:51:55 +0200

    Table example

--- src/app/modules/test/pages/test-startpage/test-startpage.component.ts
+++ src/app/modules/test/pages/test-startpage/test-startpage.component.ts
@@ -1,7 +1,54 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TableColumn} from '../../../shared/model/table-column.class';
 
 @Component({
   selector: 'app-test-startpage',
   templateUrl: './test-startpage.component.html',
 })
-export class TestStartpageComponent {}
+export class TestStartpageComponent implements OnInit {
+  public tableColumns: TableColumn[] = [];
+  public tableData: any[] = [];
+
+  public ngOnInit(): void {
+    this.initTableColumns();
+    this.initTableData();
+  }
+
+  public initTableColumns(): void {
+    this.tableColumns = [
+      new TableColumn({
+        title: 'Vorname',
+        nameInData: 'vorname',
+      }),
+      new TableColumn({
+        title: 'Nachname',
+        nameInData: 'nachname',
+      }),
+    ];
+  }
+
+  public initTableData(): void {
+    this.tableData = [
+      {
+        vorname: 'Chuck',
+        nachname: 'Norris',
+      },
+      {
+        vorname: 'Bruce',
+        nachname: 'Willis',
+      },
+      {
+        vorname: 'Angelina',
+        nachname: 'Jolie',
+      },
+      {
+        vorname: 'Arnold',
+        nachname: 'Schwarzenegger',
+      },
+      {
+        vorname: 'Scarlet',
+        nachname: 'Johansson',
+      }
+    ];
+  }
+}
